6faeaed5b5d2edef4a24c3a83138b53d037c4346
yesl-kim <yesl00k624@gmail.com>
2023-11-10 15:45:09 +0900

    fix: task 조회시 date 인자를 전달하지 않을 때 발생하는 오류 해결

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -77,7 +77,7 @@ const App = () => (
                     fields: {
                       date: {
                         read(_, { variables }) {
-                          return variables.date
+                          return variables.date || 'all'
                         },
                       },
                       duration: {
