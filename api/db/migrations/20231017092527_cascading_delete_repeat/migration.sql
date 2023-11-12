527de0d78e83935febf9bce523b5dccfdaad2124
yesl-kim <yesl00k624@gmail.com>
2023-11-12 13:03:46 +0900

    init: 배포 환경 세팅

--- api/db/migrations/20231017092527_cascading_delete_repeat/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Repeat" DROP CONSTRAINT "Repeat_taskId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Repeat" ADD CONSTRAINT "Repeat_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
